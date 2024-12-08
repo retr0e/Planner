e727efa1935da414ba961ee51584b628c8ca5622
Dawidak <dawidak@op.pl>
2024-12-08 21:26:21 +0100

    Zapytania postem

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 // Requiring modules
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const mssql = require("mssql");
 
 const port = 8080;
 const api_key = "asdfghjkl";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const sqlconfig = {
     user: 'sa',
@@ -12,7 +17,8 @@ const sqlconfig = {
     server: '172.25.0.1',
     database: 'planlekcji',
     "options": {
-        "encrypt": false // Disable encryption
+        "encrypt": false, // Disable encryption
+        
     }
 };
 
@@ -32,21 +38,60 @@ app.get("/", async (req, res) => {
 
 app.get("/select", async (req, res) => {
     console.log("SELECT Request!");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
 
     const key = req.query.key;
-        if (!key || key != api_key) {
-            return res.send("Wrong api key");
-        }
+    if (!key || key != api_key) {
+        return res.status(401).send("Wrong api key");
+    }
 
     const table = req.query.table;
     if (!table) {
-        return res.send("No table in querry");
+        return res.status(400).send("No table in querry");
     }
     
     new mssql.Request().query("SELECT * FROM "+table, (err, result) => {
         if (err) {
             console.error("Error executing query:", err);
         } else {
+            res.send(result.recordset); // Send query result as response
+            console.dir(result.recordset);
+        }
+    });
+});
+
+app.post("/", (req, res) => {
+    console.log("POST Request!");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+
+    res.status(404).send("Nothing");
+})
+
+app.post("/select", async (req, res) => {
+    console.log("SELECT POST Request!");
+    //res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+
+    const key = req.body.key;
+    if (!key || key != api_key) {
+        console.error("Wrong API key: " + key);
+        return res.status(401).send("Wrong api key");
+    }
+
+    console.log("L1");
+
+    const table = req.body.table;
+    if (!table) {
+        console.error("To table in request");
+        return res.status(400).send("No table in querry");
+    }
+
+    console.log("L2");
+    
+    new mssql.Request().query("SELECT * FROM "+table, (err, result) => {
+        if (err) {
+            console.error("Error executing query:", err);
+        } else {
+            console.log("Sending response querry object");
             res.send(result.recordset); // Send query result as response
             console.dir(result.recordset);
         }
